9ec1615128a57a3092e39358ed5bc199cf6c5084
Chris Burch <cburch@madison.k12.wi.us>
2020-01-07 09:41:15 -0600

    Better, tested version

--- plugins/MmsdHelpers/src/Controller/Component/SpecialFormatComponent.php
+++ plugins/MmsdHelpers/src/Controller/Component/SpecialFormatComponent.php
@@ -6,44 +6,97 @@ use Cake\Controller\Component;
 
 class SpecialFormatComponent extends Component
 {
-    public $result = [
-        'valid' => false,
-        'message' => string(null),
-        'formattedString' => (string)null,
-        'originalString' => (string)null,
-    ];
+    private $result = [];
 
-    public function telephoneNumber(string $oldNumber)
+    public function telephoneNumber(string $originalNumber)
     {
-        /*
-        $this->$result['originalString'] = $oldNumber;
-        if (substr($originalPhoneNumber,0,1) == '+') {
-            //$this->setInvalid(__('International phone numbers are not supported'));
-            $this->setMessage('International phone numbers are not supported');
-            $this->setValid(false);
+        $this->start($originalNumber);
+        $oldNumbers = $this->reduceToNumbers($originalNumber);
+        $numberArray = explode(' ',$oldNumbers);
+        if ($numberArray[0] == '1') {
+            array_shift($numberArray);
+        }
+
+        if ((substr($originalNumber,0,1) == '+') or (count($numberArray) > 4)) {
+            $this->setMessage(__('International phone numbers are not supported'));
             return $this->result;
         }
-        
-        $numbers = preg_replace('/\D/',' ',$oldNumber);
-        $numbers = trim(preg_replace('/\s\s+/',' ',$numbers));
-        $numberArray = explode(' ',$numbers);
+        $badAreaCodes = ['555','800','833','844','855','866','877','888','211','311','411','511','611','711','811','911'];
+        $badExchanges = ['555','911'];
+        if ((in_array($numberArray[0],$badAreaCodes)) or (in_array($numberArray[1],$badExchanges))) {
+            $this->setMessage(__('Phone number invalid'));
+            return $this->result;
+        }
 
         $newNumber = "({$numberArray[0]}){$numberArray[1]}-{$numberArray[2]}";
         if (!empty($numberArray[3])) {
             $newNumber .= "x{$numberArray[3]}";
         }
         $this->setFormattedString($newNumber);
-        if ($oldNumber != $newNumber) {
-            //$this->result['message'] = sprintf(__('The phone number %1$s was changed to %2$s. Please make sure it is correct.'),$oldNumber,$newNumber);
-            $this->setMessage(sprintf('The phone number %1$s was changed to %2$s. Please make sure it is correct.',$oldNumber,$newNumber));
+
+        $phonePattern = '/^\([2-9]\d{2}\)[2-9]\d{2}-\d{4}(x\d+)*$/';
+        if (preg_match($phonePattern,$newNumber) !== 1) {
+            $this->setMessage(__('Phone number format not recognized'));
+            return $this->result;
+        }
+
+        if ($oldNumbers != $this->reduceToNumbers($newNumber)) {
+            $this->result['message'] = sprintf(__('The phone number %1$s was changed to %2$s. Please make sure it is correct.'),$originalNumber,$newNumber);
         }
         $this->setValid(true);
         return $this->result;
-        */
-        return $oldNumber;
     }
 
-    /*
+    public function properName(string $originalName, bool $preserveCase = false)
+    {
+        $this->start($originalName);
+        $newName = $originalName;
+        $newName = str_replace(
+            ['à','á','â','ã','ä','å','æ','ç','é','ê','ë','ì','í','î','ï','ð','ñ','ò',
+             'ó','ô','õ','ö','ø','ù','ú','û','ü','ý','þ','ÿ','À','Á','Â','Ã','Ä','Å',
+             'Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò','Ó','Ô','Õ','Ö','Ø',
+             'Ù','Ú','Û','Ü','Ý','Þ','ß',],
+            ['a','a','a','a','a','a','ae','c','e','e','e','i','i','i','i','th','n','o',
+             'o','o','o','o','o','u','u','u','u','y','th','y','A','A','A','A','A','A',
+             'AE','C','E','E','E','E','I','I','I','I','TH','N','O','O','O','O','O','O',
+             'U','U','U','U','Y','TH','ss',],
+            $newName
+        );
+        $newName = preg_replace('/[^a-zA-Z \'\.\-]/','',$newName);
+        $newName = trim(preg_replace('/\s\s+/',' ',$newName));
+        
+        if ((preg_match('/[A-Z]/',$newName) === 0) or (preg_match('/[a-z]/',$newName) === 0)) {
+            if (!$preserveCase) {
+                $ucfirstName = [];
+                foreach (explode(' ',$newName) as $part) {
+                    if (stripos($part,'Mc') === 0) {
+                        $ucfirstName[] = 'Mc' . strtoupper(substr($part,2,1)) . strtolower(substr($part,3));
+                    } else {
+                        $ucfirstName[] = ucfirst(strtolower($part));
+                    }
+                }
+                $newName = implode(' ',$ucfirstName);
+            }
+        }
+        
+        if ($originalName != $newName) {
+            $this->result['message'] = __('Name was changed');
+        }
+        $this->setFormattedString($newName);
+        $this->setValid(true);
+        return $this->result;
+    }
+    
+    private function start(string $originalString)
+    {
+        $this->result = [
+            'valid' => false,
+            'originalString' => $originalString,
+            'message' => null,
+            'formattedString' => null,
+        ];
+    }
+
     private function setValid(bool $valid)
     {
         $this->result['valid'] = $valid;
@@ -60,6 +113,12 @@ class SpecialFormatComponent extends Component
     private function setFormattedString(string $str) {
         $this->result['formattedString'] = $str;
     }
-    */
-    
+
+    private function reduceToNumbers(string $str)
+    {
+        $str = preg_replace('/\D/',' ',$str);
+        $str = trim(preg_replace('/\s\s+/',' ',$str));
+        return $str;
+    }
+
 }
