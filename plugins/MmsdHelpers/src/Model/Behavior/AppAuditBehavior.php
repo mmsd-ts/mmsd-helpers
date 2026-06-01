d79e9fa1962c2ed2bce22a8c1639a09f91c8a2ac
mmsd-ts <cburch@madison.k12.wi.us>
2026-06-01 16:16:44 -0500

    Try on afterSave

---
+++ plugins/MmsdHelpers/src/Model/Behavior/AppAuditBehavior.php
@@ -0,0 +1,35 @@
+<?php
+namespace MmsdHelpers\Model\Behavior;
+use Cake\ORM\Behavior;
+use Cake\Event\EventInterface;
+use Cake\Datasource\EntityInterface;
+use Cake\ORM\Table;
+use Cake\ORM\Entity;
+use Cake\Log\Log;
+
+class AppAuditBehavior extends Behavior
+{
+    public function afterSave(EventInterface $event, EntityInterface $entity)
+    {
+        $dump = [];
+        $dump['Table'] = $this->table()->getTable();
+        $dump['Class'] = $entity->getSource();
+        $dump['Action'] = ($entity->isNew()) ? 'Insert' : 'Update';
+        $originalValues = $entity->getOriginalValues();
+        $auditedData = [
+            'old' => [],
+            'new' => [],
+        ];
+        foreach ($originalValues as $key => $originalValue) {
+            if (in_array($key,['created','modified',])) {
+                continue;
+            }
+            if ($originalValue != $entity->$key) {
+                $auditedData['old'][$key] = $originalValue;
+                $auditedData['new'][$key] = $entity->$key;
+            }
+        }
+        $dump['AuditedData'] = json_encode($auditedData);
+        Log::debug(print_r($dump,true));
+    }
+}
