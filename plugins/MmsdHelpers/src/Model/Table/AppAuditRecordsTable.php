3e9c57c42f0ce7a4559d2dec317342c35bdad5c5
mmsd-ts <cburch@madison.k12.wi.us>
2026-06-01 15:37:32 -0500

    Add Audit Table Class

---
+++ plugins/MmsdHelpers/src/Model/Table/AppAuditRecordsTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class AppAuditRecordsTable extends Table
+{
+    public function initialize(array $config): void
+    {
+        $this->setTable('dbo.AppAudit');
+    }
+}
