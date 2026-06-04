a2c7289f67cc365c78a8e30069c42a4d492183ce
mmsd-ts <cburch@madison.k12.wi.us>
2026-06-04 11:28:37 -0500

    Squashed commit of the following:
    commit ff42666b374ebb4dbc456b5966d5f1388e26aa94
    Author: mmsd-ts <cburch@madison.k12.wi.us>
    Date:   Thu Jun 4 11:13:18 2026 -0500
    
        Move writing UserData config var to component init
    
    commit 4a83063362ff37f6cc2ebf800e091ad8840229ff
    Author: mmsd-ts <cburch@madison.k12.wi.us>
    Date:   Wed Jun 3 08:46:07 2026 -0500
    
        Missing UserData message in audit record
    
    commit 3b99b3c8068ae533b7b3c6548c1b53155f5b6bc2
    Author: mmsd-ts <cburch@madison.k12.wi.us>
    Date:   Wed Jun 3 08:40:42 2026 -0500
    
        DRY writing Audit record
    
    commit af7968c1f90d2b3fb15b26eb7b61bbde54cb83ed
    Author: mmsd-ts <cburch@madison.k12.wi.us>
    Date:   Wed Jun 3 08:16:25 2026 -0500
    
        Read UserData from Config var

--- plugins/MmsdHelpers/src/Controller/Component/AppAuditComponent.php
+++ plugins/MmsdHelpers/src/Controller/Component/AppAuditComponent.php
@@ -4,38 +4,39 @@ namespace MmsdHelpers\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\ORM\Entity;
+use Cake\Core\Configure;
 
 class AppAuditComponent extends Component
 {
-    public function userData(): array
+    public function initialize(array $config): void
     {
-        $patchData = [
+        parent::initialize($config);
+        $makeValue = function (Entity $user): string|int {
+            $value = '';
+            if (!empty($user->username)) {
+                $value = $user->username;
+                if (!empty($user->fullName)) {
+                    $value .= " - {$user->fullName}";
+                }
+            } else {
+                $value = $user->id;
+            }
+            return $value;
+        };
+        $userData = [
             'audit_user' => null,
             'audit_impersonator' => null,
         ];
         if ($this->getController()->getRequest()->getSession()->check('Auth')) {
-            $patchData['audit_user'] = $this->makeValue(
+            $userData['audit_user'] = $makeValue(
                 $this->getController()->getRequest()->getSession()->read('Auth')
             );
         }
         if ($this->getController()->getRequest()->getSession()->check('AuthImpersonate')) {
-            $patchData['audit_impersonator'] = $this->makeValue(
+            $userData['audit_impersonator'] = $makeValue(
                 $this->getController()->getRequest()->getSession()->read('AuthImpersonate')
             );
         }
-        return $patchData;
-    }
-    private function makeValue(Entity $user): string|int
-    {
-        $value = '';
-        if (!empty($user->username)) {
-            $value = $user->username;
-            if (!empty($user->fullName)) {
-                $value .= " - {$user->fullName}";
-            }
-        } else {
-            $value = $user->id;
-        }
-        return $value;
+        Configure::write('App.Audit.UserData',$userData);
     }
 }
